cd74f5b5fbc818e285e0f5bfd8990bbf7d90995a
Mohamed Sami <srmf2025@gmail.com>
2024-01-25 21:33:41 +0530

    fix: add bottom margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const images = await getImages();
 
   return (
-    <main className="mt-10 flex flex-col gap-10 min-h-screen items-center justify-center">
+    <main className="my-10 flex flex-col gap-10 min-h-screen items-center justify-center">
       <Upload />
       <ListImages images={images} />
     </main>
